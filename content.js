f02ddc0ab57a2a71d01a3a30741f71f219f4bcda
Alson van der Meulen <alson@l1nda.nl>
2018-09-01 13:47:42 +0200

    Revert "Accelerometer test."
    This reverts commit 84eef430143414527a1aa7ed3ef4bd935f02c375.

--- content.js
+++ content.js
@@ -1,7 +1,11 @@
-function handleMotion (event) {
+function foo (event) {
   const myDiv = document.body;
-  
-  myDiv.style.transform = 'rotate('+(event.accelerationIncludingGravity.x*36)+'deg)';
+  console.log(myDiv);
+  /*if (myDiv.style.transform) {
+  	myDiv.style.transform = '';
+  } else*/ {
+		myDiv.style.transform = 'rotate('+(Math.floor(Math.random()*360))+'deg)';
+  }
+  console.log(myDiv.style.transform);
 }
-document.addEventListener('devicemotion', handleMotion);
-
+document.addEventListener('mousemove', foo);
