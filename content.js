87e28975f6cda92061e4a63bc77d3bd5323b8cb9
Alson van der Meulen <alson@l1nda.nl>
2018-09-01 14:39:02 +0200

    Add drunk motion

--- content.js
+++ content.js
@@ -1,11 +1,36 @@
-function foo (event) {
-  const myDiv = document.body;
-  console.log(myDiv);
-  /*if (myDiv.style.transform) {
-  	myDiv.style.transform = '';
-  } else*/ {
-		myDiv.style.transform = 'rotate('+(Math.floor(Math.random()*360))+'deg)';
-  }
-  console.log(myDiv.style.transform);
+var step = 'x';
+var t = 0;
+
+function xyas(t) {
+  const x = 5 * (Math.sin(t/45) + Math.sin(t/135));
+  const y = -3 * Math.cos(t/63);
+  const a = 10 * Math.cos(t/90);
+  const s = Math.sqrt(Math.pow(4 * Math.sin(t/320),2)) - 2;
+  return [x, y, a, s];
 }
-document.addEventListener('mousemove', foo);
+
+function nextStep (event) {
+	const myDiv = document.body;
+  const [x, y, a, s] = xyas(t);
+  switch(step) {
+    case 'x':
+      myDiv.style.left = x + 'px';
+      step = 'y';
+      break;
+    case 'y':
+      myDiv.style.top = y + 'px';
+      step = 'r';
+      break;
+    case 'r':
+      myDiv.style.transform = 'rotate('+a+'deg)';
+      step = 's';
+      break;
+    case 's':
+      myDiv.style.filter = 'blur('+s+'px)';
+      step = 'x';
+      break;
+  }
+  t += 1;
+  window.setTimeout(nextStep, 0.1);
+}
+window.setTimeout(nextStep, 0.1)
