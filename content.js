0220080412a3291c09b552fa2f4157c2463c1bf8
Alson van der Meulen <alson@l1nda.nl>
2018-09-01 11:12:44 +0200

    Add annoying rotation.

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-console.log("Hi I'm working")
+function foo (event) {
+  const myDiv = document.body;
+  console.log(myDiv);
+  /*if (myDiv.style.transform) {
+  	myDiv.style.transform = '';
+  } else*/ {
+		myDiv.style.transform = 'rotate('+(Math.floor(Math.random()*360))+'deg)';
+  }
+  console.log(myDiv.style.transform);
+}
+document.addEventListener('mousemove', foo);
